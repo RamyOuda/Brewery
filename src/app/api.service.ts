5262143fd5f0ce8ec8c007069fe13ff0c3613867
Ramy Ouda <ramyouda@gmail.com>
2022-10-28 14:55:14 -0400

    Create getData method

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  url = 'https://api.openbrewerydb.org/breweries/random';
+
+  getData() {
+    return this.http.get<any>(this.url);
+  }
 }
